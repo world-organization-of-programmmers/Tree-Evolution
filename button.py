c5f2b0ecf995f98076085850a6bcd6d7d0d2fb37
HlebPlah <glebplahtiy@gmail.com>
2020-03-20 15:00:38 +0200

    pressed animation

--- button.py
+++ button.py
@@ -10,18 +10,33 @@ class Button:
         self.rect = pygame.Rect(rect)
         self.text = text
         self.text_color = text_color
-        self.font_size = len(self.text) * 12
+        self._unpressed_font_color = text_color
+        self._pressed_font_color = tuple(map(lambda a: a // 2, text_color))
+        self._font_size = len(self.text) * 12
+        self._pressed = False
+        self._unpressed_color = color
+        self._pressed_color = tuple(map(lambda a: a // 2, color))
 
     def draw_button(self):
-        pygame.draw.rect(self.screen, self.color, self.rect)
-        font = pygame.font.SysFont(None, self.font_size)
+        pygame.draw.rect(self.screen, self.color, self.rect,)
+        font = pygame.font.SysFont(None, self._font_size)
         text = font.render(self.text, 1, self.text_color)
         self.screen.blit(text, self.rect)
 
-    def pressed(self, event):
+    def _press(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = pygame.mouse.get_pos()
             if event.button == 1 and self.rect.left < mouse_pos[
                 0] < self.rect.right and self.rect.top < mouse_pos[1] < self.rect.bottom:
-                return True
-        return False
+                self.color = self._pressed_color
+                self.text_color = self._pressed_font_color
+                self._pressed = True
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            self.color = self._unpressed_color
+            self.text_color = self._unpressed_font_color
+            self._pressed = False
+
+    def is_pressed(self, event):
+        self._press(event)
+        return self._pressed
--- game_function.py
+++ game_function.py
@@ -6,5 +6,5 @@ def check_event(button):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        if button.pressed(event):
+        if button.is_pressed(event):
             print("pressed")
