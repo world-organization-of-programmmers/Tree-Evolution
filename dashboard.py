9c05a809bd349bb5fb32115d5547f37a97b9f7e8
HlebPlah <glebplahtiy@gmail.com>
2020-03-20 17:29:52 +0200

    change button position

--- dashboard.py
+++ dashboard.py
@@ -11,11 +11,11 @@ class Dashboard:
         self.width = setting.width
         self.height = 60
         self.surface = pygame.Surface((self.width, self.height))
-        self.buttons = [Button(self.surface, (250, 250, 250), (3, 0, 234, 100), "speed +", (0, 0, 0), 70),
-                        Button(self.surface, (150, 150, 150), (243, 0, 234, 100), "speed -", (0, 0, 0), 70),
-                        Button(self.surface, (250, 250, 250), (483, 0, 234, 100), "save genom", (0, 0, 0), 50),
-                        Button(self.surface, (150, 150, 150), (723, 0, 234, 100), "new simulation", (0, 0, 0), 50)]
-        self.text_areas = [TextArea(self.surface, (250, 250, 250), (963, 0, 234, 100), "itteration: ", (0, 0, 0), 50)]
+        self.buttons = [Button(self.surface, (250, 250, 250), (3, 0, 184, 100), "speed +", (0, 0, 0), 70),
+                        Button(self.surface, (150, 150, 150), (193, 0, 184, 100), "speed -", (0, 0, 0), 70),
+                        Button(self.surface, (250, 250, 250), (383, 0, 234, 100), "save genom", (0, 0, 0), 50),
+                        Button(self.surface, (150, 150, 150), (623, 0, 254, 100), "new simulation", (0, 0, 0), 50)]
+        self.text_areas = [TextArea(self.surface, (250, 250, 250), (883, 0, 314, 100), "itteration: ", (0, 0, 0), 50)]
 
     def draw(self):
         for obj in self.buttons + self.text_areas:
