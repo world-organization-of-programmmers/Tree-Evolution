f94ef53503638dcb67da06f89b9b93ec981ec941
HlebPlah <glebplahtiy@gmail.com>
2020-03-23 13:40:48 +0200

    post main loop code in functions, add def of key press

--- main.py
+++ main.py
@@ -9,10 +9,15 @@ from dashboard.dashboard import Dashboard
 import service.game_function as gf
 
 pygame.font.init()
+
 setting = Setting
 
 screen = pygame.display.set_mode((setting.width, setting.height))  # создание экрана
 field = Field(screen, setting)
+dashboard = Dashboard(screen, setting)
+
+itteration = 0
+delay = 0
 
 g = np.array(
     [[13, 30, 14, 12],
@@ -33,34 +38,15 @@ g = np.array(
      [30, 30, 30, 9]])
 trees = [Tree(15, 119, 0), Tree(30, 119, 0), Tree(45, 119, 0), Tree(90, 119, 0), Tree(75, 119, 0), Tree(150, 119, 0),
          Tree(105, 119, 0)]
-dashboard = Dashboard(screen, setting)
-itteration = 0
 
 while True:
-
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        for button in dashboard.buttons:
-            button.is_pressed(event)
-
-    field.fill()
-    dashboard.draw()
-    dashboard.text_areas[0].text = "itteration: " + str(itteration)
-    dashboard.blit((0, 0))
-
-    for tree in trees:
-        field.draw_pixels(tree.get_pixels())
+        gf.check_event(event, dashboard)
+        delay = gf.change_speed(event, dashboard, delay)
+    trees = gf.tree_event(trees)
+    gf.draw_objects(field, dashboard, trees, itteration)
 
     pygame.display.flip()
-
-    new_trees = []
-    for tree in trees:
-        new_tree = tree.grow(trees + new_trees)
-        new_trees += new_tree
-    trees = new_trees
-    if not trees:
-        trees = [Tree(15, 119, 0), Tree(30, 119, 0), Tree(45, 119, 0), Tree(90, 119, 0)]
     itteration += 1
-
-    # pygame.time.wait(50)
+    pygame.time.wait(delay)
+    print(delay)
--- service/game_function.py
+++ service/game_function.py
@@ -1,12 +1,35 @@
 import pygame
 import sys
+from dashboard.button import Button
 
 
-def check_event(button):
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        if button.is_pressed(event):
-            print("pressed")
+def check_event(event, dashboard, ):
+    if event.type == pygame.QUIT:
+        sys.exit()
 
 
+def change_speed(event, dashboard, delay):
+    if dashboard.buttons[0].is_pressed(event):
+        if delay > 0:
+            delay -= 10
+    if dashboard.buttons[1].is_pressed(event):
+        delay += 10
+    return delay
+
+
+def tree_event(trees):
+    new_trees = []
+    for tree in trees:
+        new_tree = tree.grow(trees + new_trees)
+        new_trees += new_tree
+    return new_trees
+
+
+def draw_objects(field, dashboard, trees, itteration):
+    field.fill()
+    dashboard.draw()
+    dashboard.text_areas[0].text = "itteration: " + str(itteration)
+    dashboard.blit((0, 0))
+
+    for tree in trees:
+        field.draw_pixels(tree.get_pixels())
